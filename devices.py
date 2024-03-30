b98d002d78c47d68f70b4c0fad1b1623439ba3fb
liuzeli <liuzeli0708@gmail.com>
2024-03-29 22:47:47 -0700

    update devices

--- devices.py
+++ devices.py
@@ -3,7 +3,7 @@ from typing import Tuple, Union
 import torch
 
 # The torch.device to use for computation
-DEVICE = None
+DEVICE = "cuda"
 
 def forward_pre_hook_to_device(_module, inputs) \
     -> Union[Tuple[torch.tensor], Tuple[Tuple[torch.Tensor]]]:
