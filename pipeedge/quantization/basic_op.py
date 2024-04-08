b6e198b740553f7f6c0d9719a843a1deab900714
liuzeli <liuzeli0708@gmail.com>
2024-04-08 00:13:23 -0700

    e=3 with clamp

--- pipeedge/quantization/basic_op.py
+++ pipeedge/quantization/basic_op.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 def _quant_op(input_data, bit, mode='original'):
-    e=2
+    e=3
     m=bit-e-1
     sign=np.sign(input_data).flatten()
     sign=np.where(sign>=0,0,1)
