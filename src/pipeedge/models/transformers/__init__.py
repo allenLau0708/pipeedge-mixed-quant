cf639946db1fee21ee5f5cc157b16b3e02d152bc
liuzeli <liuzeli0708@gmail.com>
2024-04-08 23:10:31 -0700

    delete src folder

--- src/pipeedge/models/transformers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Transformers module."""
-from typing import Tuple, Type, Union
-from torch import Tensor
-
-TransformerShardData: Type = Union[Tensor, Tuple[Tensor, Tensor]]
-"""A transformer shard input/output type."""
