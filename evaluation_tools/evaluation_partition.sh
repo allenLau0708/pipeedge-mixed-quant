6a1e5adcda00e9e04ea26353a2ee55b427ccf00c
liuzeli <liuzeli0708@gmail.com>
2024-05-16 02:20:58 -0700

    vit large noClamp

--- evaluation_tools/evaluation_partition.sh
+++ evaluation_tools/evaluation_partition.sh
@@ -38,8 +38,8 @@ do
 		for pt in `seq 1 95`
 		do
 			pt=$(generatePartition $pt)
-			sbatch upload_eval_clamp.job $pt $bit $e
-# 			sbatch upload_eval_noClamp.job $pt $bit $e
+			# sbatch upload_eval_clamp.job $pt $bit $e
+			sbatch upload_eval_noClamp.job $pt $bit $e
 		done
 	done
 done
